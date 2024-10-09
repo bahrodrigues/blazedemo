c08d3c3b5b7a9774a4a46b9fd7e88d3635a825d3
bahrodrigues <bah.rodrigues222@gmail.com>
2024-10-08 22:28:26 -0300

    first commit

---
+++ src/test/java/passagemTest.java
@@ -0,0 +1,77 @@
+// 1 - imports  bibliotecas
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver; //biblioteca principal do selenium
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+// 2 - classe informacoes
+
+public class passagemTest {
+    
+    //2.1 atributos
+    private WebDriver driver; //objeto do selenium
+
+    //2.2 funções e métodos
+
+    //antes do teste
+    @BeforeEach
+    public void setup() {
+        driver = new ChromeDriver(); // instanciar o onbjeto do selenium como ChromeDriver
+        driver.manage().window().maximize(); // maxima a janela do browser
+    }
+
+    //depois do teste
+    @AfterEach 
+    public void finish() {
+        driver.quit(); // destroi o objeto selenium
+    }
+    
+    //teste
+    @Test
+    public void comprarPassagem() {
+        driver.get("https://blazedemo.com/"); //abre o site blazedemo
+        // seleciona origem, destino e aperta o botão "Find flights"
+
+        // origem
+        driver.findElement(By.name("fromPort")).click();
+        {
+            WebElement dropdown = driver.findElement(By.name("fromPort"));
+            dropdown.findElement(By.xpath("//option[. = 'São Paolo']")).click();
+        }
+        
+        // destino
+        {
+            WebElement dropdown = driver.findElement(By.name("toPort"));
+            dropdown.click();
+            dropdown.findElement(By.xpath("//option[. = 'Cairo']")).click();
+        }
+ 
+        // clicar no botão 'find fligths'
+        driver.findElement(By.cssSelector("input.btn.btn-primary")).click();
+
+        // transição de página
+
+        // verifica se foi feito o login e se trocou de pagina
+        assertEquals("Flights from São Paolo to Cairo:", driver.findElement(By.cssSelector("h3")).getText());
+
+        driver.findElement(By.cssSelector("tr:nth-child(2) .btn")).click();
+
+        //transicao de pagina
+
+        assertEquals("Your flight from TLV to SFO has been reserved.", driver.findElement(By.cssSelector("h2")).getText()); 
+
+        driver.findElement(By.name("inputName")).sendKeys("Bárbara Rodrigues");
+        driver.findElement(By.name("address")).sendKeys("Rua dos Caracois, 123");
+        driver.findElement(By.name("city")).sendKeys("Caracol");
+        driver.findElement(By.name("state")).sendKeys("MG");
+        driver.findElement(By.name("zipCode")).sendKeys("54321");
+        WebElement cardtype = driver.findElement(By.name("cardType"));
+        driver.findElement(By.name("state")).sendKeys("MG");
+        
+    } // final da classe passagemTest
+
+} //
